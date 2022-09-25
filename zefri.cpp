6f4b84310ae2e4d5a783c6c4c95ac3f625719fde
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2022-09-25 11:33:32 +0700

    Updating
    Updating Data

--- zefri.cpp
+++ zefri.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
-    cout<<"Nama Saya Zefri";
+    cout<<"Nama Saya Zefri"<<endl;
+    cout<<"5 + 5 \n ";
+    cout<<5 + 5 <<endl;
+    cout<<"1 + 1 \n ";
+    cout<<1 + 1 <<endl;
 }
